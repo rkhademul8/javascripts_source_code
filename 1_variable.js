6b99b8ab51277dfb444e534950f73baa276a6281
rkhademul8 <75939920+rkhademul8@users.noreply.github.com>
2021-08-13 23:41:24 +0600

    variable

---
+++ 1_variable.js
@@ -0,0 +1,17 @@
+
+// There are three type of variable in js var,let and const 
+
+
+// This the publick key 
+var name="Khademul";
+console.log(name);
+
+// For let variable keyword we can update the variable value
+let number1=5;
+number1=10
+console.log(number1);
+
+// we can not change the variable value 
+const id=17103335;
+console.log(id);
+
